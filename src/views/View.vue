c4b2cf83c18abb44064b1672d873d4122f00de94
Evotianus <evotianus0110@gmail.com>
2025-03-03 19:21:38 +0700

    feat: view page

--- src/views/View.vue
+++ src/views/View.vue
@@ -1,6 +1,98 @@
 <template>
-    <div>
-        <div class="text-2xl">View Page</div>
+    <div class="flex justify-center">
+        <div class="content px-8 md:px-12 lg:px-32 xl:px-48 py-4 w-full">
+            <div class="flex flex-col justify-center items-center w-full gap-4">
+                <span class="text-lg font-semibold">Next Chapter</span>
+                <div class="bg-gray-50 ring-1 ring-gray-500/10 ring-inset rounded-lg p-2 flex items-center">
+                    <div class="flex items-center gap-4">
+                        <img src="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ixfNTiyYtG1c/v0/459x306.webp" alt=""
+                            class="h-full w-36 rounded-lg">
+                        <div class="flex flex-col">
+                            <p class="font-semibold text-lg">Chapter 100</p>
+                            <span>Feb 27, 2025</span>
+                        </div>
+                    </div>
+                </div>
+                <button class="bg-gray-200 w-fit px-4 py-2 rounded-full font-semibold mt-2">
+                    Options
+                </button>
+                <div class="comic-socials grid grid-cols-2 gap-3 w-full">
+                    <div class="col-span-2 hidden md:block">
+                        <div
+                            class="p-2 py-3 h-16 bg-gray-50 ring-1 ring-gray-500/10 ring-inset rounded-lg flex items-center">
+                            <div
+                                class="divider mx-2 w-1 h-full rounded-full bg-gray-200 ring-1 ring-gray-500/10 ring-inset">
+                            </div>
+                            <div class="flex flex-col">
+                                <p>Share Kumapage</p>
+                                <span class="text-sm">to your friends</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-2 lg:col-span-1">
+                        <div
+                            class="p-2 py-3 h-16 bg-gray-50 ring-1 ring-gray-500/10 ring-inset rounded-lg flex items-center justify-between">
+                            <div class="h-full flex items-center">
+                                <div
+                                    class="divider mx-2 mr-4 w-1 h-full rounded-full bg-gray-200 ring-1 ring-gray-500/10 ring-inset">
+                                </div>
+                                <div class="flex flex-col">
+                                    <p class="text-lg">Share Kumapage</p>
+                                    <span class="text-sm">to your friends</span>
+                                </div>
+                            </div>
+                            <button class="px-4 py-2 bg-red-400 rounded-full text-white flex items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    class="icon icon-tabler icons-tabler-filled icon-tabler-alert-hexagon">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path
+                                        d="M10.425 1.414a3.33 3.33 0 0 1 3.026 -.097l.19 .097l6.775 3.995l.096 .063l.092 .077l.107 .075a3.224 3.224 0 0 1 1.266 2.188l.018 .202l.005 .204v7.284c0 1.106 -.57 2.129 -1.454 2.693l-.17 .1l-6.803 4.302c-.918 .504 -2.019 .535 -3.004 .068l-.196 -.1l-6.695 -4.237a3.225 3.225 0 0 1 -1.671 -2.619l-.007 -.207v-7.285c0 -1.106 .57 -2.128 1.476 -2.705l6.95 -4.098zm1.585 13.586l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" />
+                                </svg>
+                                <span>Report</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-span-2 lg:col-span-1">
+                        <div
+                            class="p-2 py-3 h-16 bg-gray-50 ring-1 ring-gray-500/10 ring-inset rounded-lg flex items-center justify-between">
+                            <div class="h-full flex items-center">
+                                <div
+                                    class="divider mx-2 w-1 h-full rounded-full bg-gray-200 ring-1 ring-gray-500/10 ring-inset">
+                                </div>
+                                <div class="flex flex-col">
+                                    <p>Share Kumapage</p>
+                                    <span class="text-sm">to your friends</span>
+                                </div>
+                            </div>
+                            <button class="px-4 py-2 bg-red-400 rounded-full text-white flex items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    class="icon icon-tabler icons-tabler-filled icon-tabler-alert-hexagon">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path
+                                        d="M10.425 1.414a3.33 3.33 0 0 1 3.026 -.097l.19 .097l6.775 3.995l.096 .063l.092 .077l.107 .075a3.224 3.224 0 0 1 1.266 2.188l.018 .202l.005 .204v7.284c0 1.106 -.57 2.129 -1.454 2.693l-.17 .1l-6.803 4.302c-.918 .504 -2.019 .535 -3.004 .068l-.196 -.1l-6.695 -4.237a3.225 3.225 0 0 1 -1.671 -2.619l-.007 -.207v-7.285c0 -1.106 .57 -2.128 1.476 -2.705l6.95 -4.098zm1.585 13.586l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" />
+                                </svg>
+                                <span>Report</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="comic-comments bg-gray-200 h-96 rounded-lg flex justify-center items-center w-full">
+                    <button
+                        class="bg-gray-50 ring-1 ring-gray-500/10 ring-inset flex items-center gap-2 rounded-lg px-6 py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="icon icon-tabler icons-tabler-outline icon-tabler-messages">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
+                            <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
+                        </svg>
+                        <span class="text-lg font-semibold">Click to load comments</span>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
